b811ec93a819864d9fda4cf7a83e28d31bbe1553
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-05-01 17:18:19 +0300

    api ekleme ve verileri çekme

--- app/todo-list.js
+++ app/todo-list.js
@@ -1,11 +1,26 @@
-import React from 'react'
+const getTodos = async () => {
+  let todos = await fetch ("http://localhost:3001/api/todo/list");
+  return todos.json();
+}
 
-const ToDoList = () => {
+export default async function ToDoList () {
+const {todos} = await getTodos() ;
+
+
   return (
     <div>
-        To Do List
+       <ul style={{listStyleType: "none", padding: 0}}>
+      {todos.map((t) => {
+        return(
+    <li key={t.id} style={{padding: "5px 0"}}>
+      <input type="checkbox" />
+      {t.name}
+      <button>Delete</button>
+    </li>
+        )
+      })}
+       </ul>
     </div>
   )
 }
 
-export default ToDoList
